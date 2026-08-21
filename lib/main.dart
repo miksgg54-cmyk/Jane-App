4a8a37ecb792a2587baa395233a4ba90a7b624fa
miksgg54-cmyk <316254341+miksgg54-cmyk@users.noreply.github.com>
2026-08-21 18:53:27 -0400

    Initialize Flutter app with success message
    Added a Flutter app with a success message in Arabic.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
 
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
+        body: const Center(
+          child: Text(
+            'تطبيق جين يعمل بنجاح! 🚀',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
